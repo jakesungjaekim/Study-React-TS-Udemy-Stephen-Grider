f7ba27438f5e30a3b11f52c203a4b4a937c5f967
jakesungjaekim <codejakecarry@gmail.com>
2023-03-27 18:08:58 +0900

    ADD: useRef

--- rts/src/main.tsx
+++ rts/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import EventComponent from './events/EventComponent'
+import UserSearch from './state/UserSearch'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <EventComponent />
+    <UserSearch />
   </React.StrictMode>,
 )
--- rts/src/state/UserSearch.tsx
+++ rts/src/state/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const users = [
   {
@@ -14,8 +14,16 @@ const users = [
 
 
 const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null)
   const [name, setName] = useState('')
   const [user, setUser] = useState<{ name: string, age: number | undefined}>();
+
+  useEffect(()=>{
+    if(!inputRef.current){
+      return;
+    }
+    inputRef.current.focus()
+  },[])
 
   const onClick = () => {
     const foundUser = users.find((user)=>{
@@ -29,7 +37,7 @@ const UserSearch: React.FC = () => {
   return (
     <div>
       User Search
-      <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/>
+      <input ref={inputRef} type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/>
       <button onClick={onClick}>Find User</button>
       <div>
         {user && user.name}
